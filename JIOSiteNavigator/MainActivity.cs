a3353ef49c96f9d2f9348ef692fe25faa3873725
susunkar <suresh.kumars@live.com>
2017-06-29 11:47:43 +0530

    V1  tab creation done

--- JIOSiteNavigator/MainActivity.cs
+++ JIOSiteNavigator/MainActivity.cs
@@ -1,18 +1,37 @@
-﻿using Android.App;
-using Android.Widget;
-using Android.OS;
-
-namespace JIOSiteNavigator
+﻿namespace JIOSiteNavigator
 {
-    [Activity(Label = "JIOSiteNavigator", MainLauncher = true, Icon = "@drawable/icon")]
-    public class MainActivity : Activity
+    using System;
+    using Android.App;
+    using Android.Content;
+    using Android.Graphics.Drawables;
+    using Android.OS;
+    using Android.Widget;
+    
+    [Activity(Label = "JIO Site Navigator", MainLauncher = true, Icon = "@drawable/CellTower")]
+    public class MainActivity : TabActivity
     {
         protected override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
 
             // Set our view from the "main" layout resource
-            // SetContentView (Resource.Layout.Main);
+            SetContentView (Resource.Layout.Main);
+
+            CreateTab(typeof(SiteSearchActivity), "Search", "Search",Resource.Drawable.ic_tab_searche);
+            CreateTab(typeof(AboutActivity), "About", "About", Resource.Drawable.ic_tab_about);
+        }
+
+        private void CreateTab(Type activityType, string tag, string label, int drawableId)
+        {
+            var intent = new Intent(this, activityType);
+            intent.AddFlags(ActivityFlags.NewTask);
+
+            var spec = TabHost.NewTabSpec(tag);
+            var drawableIcon = Resources.GetDrawable(drawableId);
+            spec.SetIndicator(label, drawableIcon);
+            spec.SetContent(intent);
+
+            TabHost.AddTab(spec);
         }
     }
 }
